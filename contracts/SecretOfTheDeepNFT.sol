aa7d2f9216f38bfbbbd2415b75eeb31bcd214476
TomasNahlik <tomasvtp@gmail.com>
2026-06-25 00:51:54 +0200

    Move NFT metadata to EsantiGames repository

--- contracts/SecretOfTheDeepNFT.sol
+++ contracts/SecretOfTheDeepNFT.sol
@@ -57,7 +57,7 @@ contract SecretOfTheDeepNFT is ERC1155, Ownable, IERC4906 {
     string private _baseURI;
     
     // Contract URI for collection metadata
-    string private _contractURI = "https://raw.githubusercontent.com/davevurby/nft-secret-of-the-deep/refs/heads/main/metadata/contract.json";
+    string private _contractURI = "https://raw.githubusercontent.com/EsantiGames/nft-secret-of-the-deep/main/metadata/contract.json
     
     // USDC token address (Polygon Native USDC)
     address public usdcAddress = 0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359;
@@ -68,8 +68,7 @@ contract SecretOfTheDeepNFT is ERC1155, Ownable, IERC4906 {
     event DividendPaid(address indexed to, uint256 indexed amount);
     
     constructor() ERC1155("") Ownable(msg.sender) {
-        _baseURI = "https://raw.githubusercontent.com/davevurby/nft-secret-of-the-deep/refs/heads/main/metadata/{id}.json";
-        
+        _baseURI = "https://raw.githubusercontent.com/EsantiGames/nft-secret-of-the-deep/main/metadata/{id}.json
         // Initialize some sample tokens
         _createToken(1, "GOLD", "Gold", 25);
         _createToken(2, "SILVER", "Silver", 20);
